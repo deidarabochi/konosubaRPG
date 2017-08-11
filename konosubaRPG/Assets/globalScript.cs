f8cd6a4809e91d4ab60510142c426b8d1c38c1e8
Junha Park <junha923@yahoo.com>
2017-08-11 15:36:17 -0700

    081117 1535
    4 shy guys

---
+++ konosubaRPG/Assets/globalScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class globalScript : MonoBehaviour
+{
+    public int controllerNumber;
+
+    // Use this for initialization
+    void Start()
+    {
+        controllerNumber = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1) == true)
+        {
+            controllerNumber = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2) == true)
+        {
+            controllerNumber = 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3) == true)
+        {
+            controllerNumber = 3;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4) == true)
+        {
+            controllerNumber = 4;
+        }
+    }
+}
