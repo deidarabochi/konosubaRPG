0f27ff7a81415fe4396d23122385d36dd50cbe7f
deidarabochi <junha923@yahoo.com>
2017-08-13 19:33:03 -0700

    a* v2.3
    managed to get player control working. right-click on the map and the white square will find a way there.
    
    problems: need to fix when a collision object is clicked

---
+++ aStarV2.3/Assets/Target.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+	private float clickX;
+	private float clickY;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (1)) {
+			Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);//Obtains world coordinates of right-clicked area
+			clickX = Mathf.Round(pos.x);
+			clickY = Mathf.Round(pos.y);
+			transform.position = new Vector3(clickX, clickY, 0);
+		}
+	}
+}
